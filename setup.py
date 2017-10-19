aa7b6cb8433f13115a0d08125252ce325a30c89b
Chad Dombrova <chadrik@gmail.com>
2017-10-19 14:09:28 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doc484",
+    version="0.0.1",
+    author="Chad Dombrova",
+    description="Utilities for working with PEP484 types within docstrings",
+    license="BSD",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
+    packages=find_packages(),
+    # long_description=read('README'),
+    # classifiers=[
+    #     "Development Status :: 3 - Alpha",
+    #     "Topic :: Utilities",
+    #     "License :: OSI Approved :: BSD License",
+    # ],
+)
