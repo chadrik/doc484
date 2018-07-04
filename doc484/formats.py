9570550c1d7f167531d8ef845e93a738b26c0aa7
Chad Dombrova <chadrik@gmail.com>
2018-07-03 23:09:42 -0400

    prepare for 0.2.0

--- doc484/formats.py
+++ doc484/formats.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, print_function
+
 import re
 import inspect
 import logging
--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@ import os.path
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+
 def read(*parts):
     with open(os.path.join(HERE, *parts)) as f:
         return f.read()
 
+
 setup(
     name="doc484",
-    version="0.1.0",
+    version="0.2.0",
     author="Chad Dombrova",
     description="Generate PEP 484 type comments from docstrings",
+    long_description=read("README.rst"),
     license="MIT",
-    keywords=["mypy", "typing", "pep484", "docstrings"],
+    keywords=["mypy", "typing", "pep484", "docstrings", "annotations"],
     url="https://github.com/chadrik/doc484",
     packages=find_packages(),
     entry_points={
@@ -29,4 +32,25 @@ setup(
             "tox==2.7.0",
         ],
     },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
