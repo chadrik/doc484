597b421a398f5afcc5feb7abae376820fcc25876
Chad Dombrova <chadrik@gmail.com>
2019-04-17 22:17:06 -0400

    Update version to 0.3.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*parts):
 
 setup(
     name="doc484",
-    version="0.3.3",
+    version="0.3.4",
     author="Chad Dombrova",
     description="Generate PEP 484 type comments from docstrings",
     long_description=read("README.rst"),
